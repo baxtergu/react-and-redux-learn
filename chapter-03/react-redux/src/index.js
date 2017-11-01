f3d9e28c9a7d629f3977a2166cda1769ab71c13a
baxtergu <baxtergu@gmail.com>
2017-11-01 15:13:28 +0800

    使用react_context中的代码，未修改，尚不能运行

--- chapter-03/react-redux/src/index.js
+++ chapter-03/react-redux/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ControlPanel from './views/ControlPanel';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import store from './Store.js';
+import Provider from './Provider.js';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ControlPanel />
+  </Provider>,
+  document.getElementById('root')
+);
