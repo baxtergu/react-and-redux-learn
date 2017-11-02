30f34870b1722b5d8f072ff25530ed82689d6ab0
baxtergu <baxtergu@gmail.com>
2017-11-03 00:01:42 +0800

    使用selector来改造代码完成

--- chapter-05/todo_with_selector/src/todos/views/todoList.js
+++ chapter-05/todo_with_selector/src/todos/views/todoList.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 import TodoItem from './todoItem';
 import { FilterTypes } from '../../constants';
+import { selectVisibleTodos } from '../selector';
 
 const TodoList = ({ todos }) => {
     return (
@@ -26,22 +27,22 @@ TodoList.propTypes = {
     todos: PropTypes.array.isRequired
 };
 
-const selectVisibleTodos = (todos, filter) => {
-    switch (filter) {
-        case FilterTypes.ALL:
-            return todos;
-        case FilterTypes.COMPLETED:
-            return todos.filter(item => item.completed);
-        case FilterTypes.UNCOMPLETED:
-            return todos.filter(item => !item.completed);
-        default:
-            throw new Error('unsupported filter');
-    }
-}
+// const selectVisibleTodos = (todos, filter) => {
+//     switch (filter) {
+//         case FilterTypes.ALL:
+//             return todos;
+//         case FilterTypes.COMPLETED:
+//             return todos.filter(item => item.completed);
+//         case FilterTypes.UNCOMPLETED:
+//             return todos.filter(item => !item.completed);
+//         default:
+//             throw new Error('unsupported filter');
+//     }
+// }
 
 const mapStateToProps = (state) => {
     return {
-        todos: selectVisibleTodos(state.todos, state.filter)
+        todos: selectVisibleTodos(state)
     };
 };
 
