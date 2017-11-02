5bad6a5d7fb73e67607f44cb09006fb467baa176
baxtergu <baxtergu@gmail.com>
2017-11-02 23:48:26 +0800

    初始化react_with_selector项目目录

---
+++ chapter-05/todo_with_selector/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TodoApp from './TodoApp';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import store from './Store';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <TodoApp />
+    </Provider>,
+    document.getElementById('root')
+);
+registerServiceWorker();
