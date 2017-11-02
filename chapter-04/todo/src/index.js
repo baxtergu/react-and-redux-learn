052b9af1d16b54fe1971d472972eb491cc3615b9
baxtergu <baxtergu@gmail.com>
2017-11-02 14:08:52 +0800

    完成代码基本功能，添加样式

--- chapter-04/todo/src/index.js
+++ chapter-04/todo/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import TodoApp from './TodoApp';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import store from './Store';
 
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <TodoApp />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
