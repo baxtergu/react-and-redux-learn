3ab2d327d5b3b16b2146bd0b92d5c358e371279a
baxtergu <baxtergu@gmail.com>
2017-11-07 09:54:02 +0800

    添加依赖包

--- chapter-06/hoc/src/proxy/ConnectHOC.js
+++ chapter-06/hoc/src/proxy/ConnectHOC.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const doNothing = () => ({});
 
@@ -68,7 +69,7 @@ function connect(mapStateToProps=doNothing, mapDispatchToProps=doNothing) {
     };
 
     HOCComponent.contextTypes = {
-      store: React.PropTypes.object
+      store: PropTypes.object
     }
 
     HOCComponent.displayName = `Connect(${getDisplayName(WrappedComponent)})`;
